fd2f2d80a8c53712def00623185c9a81ed5d38d8
umamaheswari <numamaheswari02@gmail.com>
2023-12-14 16:01:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const port = 4000;
+const products = require("./products.json");
+
+app.get("/",(request,response) => {
+    response.send("Welcome, I am Main Page!");
+})
+
+app.get("/products",(request,response) => {
+    response.send(products);
+})
+
+app.get("/getRandomProduct",(request,response) => {
+    const i = Math.floor(Math.random()*products.length);
+    response.send(products[i]);
+})
+
+app.listen(port, () => {
+    console.log(`Server Running on: ${port}`);
+})
